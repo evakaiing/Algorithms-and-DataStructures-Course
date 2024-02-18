bc1622634a9220fd817194e817e6ba852d400274
- - <>
2024-02-18 13:49:21 +0300

    done aplusb

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -1,5 +1,12 @@
+#include <climits>
 #include <iostream>
 
 int Sum(int a, int b) {
-    // Not implemented
+    if ((a == INT_MAX) && (b == INT_MIN + 1)) {
+        return 0;
+    } else if ((a == INT_MAX && b > 0) || (b == INT_MAX && a > 0)) {
+        return INT_MAX;
+    } else {
+        return a + b;
+    }
 }
