24596fca5d33c1f9c6bdde4c4b2a65a927476d62
evakaiing <zaitovaeva@mail.ru>
2024-03-05 12:12:44 +0300

    Bump task bash/bash

--- tasks/bash/bash/solution.py
+++ tasks/bash/bash/solution.py
@@ -2,30 +2,38 @@ import os
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(
-    description='Concatenate the specified file a specified number of times so that the length '
-                'of the resulting file in bytes does not exceed the specified number.')
-parser.add_argument('FILE', type=str, help='File for concatenation')
-parser.add_argument('REPETITION', type=int, help='Number of file concatenations')
-parser.add_argument('MAXSIZE', type=int, help='The maximum size of the resulting file')
 
-args = parser.parse_args()
+def get_arguments():
+    parser = argparse.ArgumentParser(
+            description='Concatenate the specified file a specified number of times so that the length '
+                        'of the resulting file in bytes does not exceed the specified number.')
+    parser.add_argument('FILE', type=str, help='File for concatenation')
+    parser.add_argument('REPETITION', type=int, help='Number of file concatenations')
+    parser.add_argument('MAXSIZE', type=int, help='The maximum size of the resulting file')
+    return parser.parse_args()
 
-if os.path.getsize(args.FILE) == 0:
-    sys.exit(-1)
 
-with open(args.FILE, 'r', encoding="UTF-8") as file:
-    in_file = file.readline()
+def error_processing(args):
+    if os.path.getsize(args.FILE) == 0:
+        sys.exit(-1)
+
+
+def concatenate(args):
+    error_processing(args)
+    with open(args.FILE, 'r', encoding="UTF-8") as file:
+        in_file = file.read()
+
     current_size = os.path.getsize(args.FILE)
-    size = current_size
-
-with open(args.FILE, 'a', encoding="UTF-8") as file:
-    for _ in range(args.REPETITION):
-        if current_size + size <= args.MAXSIZE:
+    while current_size + len(in_file) * args.REPETITION <= args.MAXSIZE:
+        with open(args.FILE, 'a', encoding="UTF-8") as file:
             file.write(in_file)
-            current_size += size
-        else:
-            open(args.FILE, 'w').close()
-            sys.exit(-1)
+        current_size = os.path.getsize(args.FILE)
 
 
+def main():
+    args = get_arguments()
+    concatenate(args)
+
+
+if __name__ == '__main__':
+    main()
